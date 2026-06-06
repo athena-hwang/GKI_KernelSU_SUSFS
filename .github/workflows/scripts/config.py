0c986c4540a9666779c8d9e29d118b5f9970d272
athena-hwang <53941393+athena-hwang@users.noreply.github.com>
2026-06-06 12:49:30 +0800

    Update config.py

--- .github/workflows/scripts/config.py
+++ .github/workflows/scripts/config.py
@@ -66,7 +66,7 @@ ANDROID_KERNEL_MAP = {
 # 仓库配置
 KSU_REPO_CONFIG = {"repo_url": "https://github.com/ReSukiSU/ReSukiSU.git",
                     "branch": "main",
-                    "setup_script": "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/refs/heads/main/kernel/setup.sh"}
+                    "setup_script": "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh"}
 
 # SUSFS 仓库配置
 SUSFS_REPO_CONFIG = {"repo_url": "https://github.com/ShirkNeko/susfs4ksu.git"}
